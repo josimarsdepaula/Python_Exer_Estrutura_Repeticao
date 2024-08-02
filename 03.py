fcd977d59affe4e6b372923a6f5be49f28df4c90
Josimar <josimarsdepaula@gmail.com>
2024-08-02 17:02:48 -0300

    Alterações

--- 03.py
+++ 03.py
@@ -2,7 +2,31 @@ nome = input("Informe seu nome: ")
 while len(nome) <= 3:
     print("Nome Inválido. Insira novamente acima de 3 caracteres.")
     nome = input("Informe seu nome: ")
-    
-idade = input("Informe sua Idade: ")
+
+idade = int(input("Informe sua Idade: "))
+while not idade > 0 and idade >= 150:
+    print("Idade inválida. Insira novamente entre 0 e 150 anos.") 
+    idade = input("Informe sua Idade: ")
+
+salario = float(input("Informe seu sálario: "))
+while salario < 0:
+    print("salário inválido. Insira novamente.")
+    salario = float(input("Informe seu sálario: "))
+
+sexo = input("Informe qual o seu sexo (F) - Feminino ou (M) - Masculino: ")
+while len(sexo) > 1 and sexo not in ("m","f"):
+    print("Sexo inválido. Insira novamente.")
+    sexo = input("Informe qual o seu sexo (F) - Feminino ou (M) - Masculino: ")
+
+lista_estado_civil = [
+    "(S) - Solteiro(a)",
+    "(C) - Casado(a)",
+    "(V) - Viúvo(a)",
+    "(D) - Divorciado(a)"
+]
+estado_civil = input(f"Informe seu estado civil: {lista_estado_civil}")
+
+
+
 
 
