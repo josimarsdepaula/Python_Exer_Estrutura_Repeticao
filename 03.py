c835ea09e9ebb1f17168cbf94705d2e19b9959a9
Josimar <josimarsdepaula@gmail.com>
2024-08-05 12:36:48 -0300

    teste

--- 03.py
+++ 03.py
@@ -4,19 +4,19 @@ while len(nome) <= 3:
     nome = input("Informe seu nome: ")
 
 idade = int(input("Informe sua Idade: "))
-while not idade > 0 and idade >= 150:
+while idade < 0 or idade > 150:
     print("Idade inválida. Insira novamente entre 0 e 150 anos.") 
-    idade = input("Informe sua Idade: ")
+    idade = int(input("Informe sua Idade: "))
 
 salario = float(input("Informe seu sálario: "))
 while salario < 0:
     print("salário inválido. Insira novamente.")
     salario = float(input("Informe seu salário: "))
 
-sexo = input("Informe qual o seu sexo (F) - Feminino ou (M) - Masculino: ")
-while len(sexo) > 1 and sexo not in ("m", "f"):
+sexo = input("Informe qual o seu sexo (F) - Feminino ou (M) - Masculino: ").strip().upper()
+while sexo not in ("M", "F"):
     print("Sexo inválido. Insira novamente.")
-    sexo = input("Informe qual o seu sexo (F) - Feminino ou (M) - Masculino: ")
+    sexo = input("Informe qual o seu sexo (F) - Feminino ou (M) - Masculino: ").strip().upper()
 
 lista_estado_civil = [
     "(C) - Casado(a)",
@@ -32,6 +32,19 @@ estado_civil = input(f"Informe a letra conforme seu estado civil: ").strip().upp
 while estado_civil not in ("S", "C", "V", "D"):
     print("Estado Civil inválido. Insira novamente.")
     estado_civil = input(f"Informe a letra conforme seu estado civil: ").strip().upper()
+
+estado_civil_descricao = {
+    "S": "Solteiro(a)",
+    "C": "Casado(a)",
+    "V": "Viúvo(a)",
+    "D": "Divorciado(a)"
+}
+
+print(f"Seu nome é: {nome}")
+print(f"Sua idade é: {idade}")
+print(f"Seu salário é R${salario:.2f}")
+print(f"Seu Sexo é: {'Masculino' if sexo == 'M' else 'Feminino'}")
+print(f"Seu estado civil é: {estado_civil_descricao[estado_civil]}")
 
 
 
